2544ffe26479d13b3ffbe160f3fd616bd661e689
Andrey <andrii.tykhanovych.pl@gmail.com>
2020-07-04 15:13:17 +0200

    Advise class JSON class
    Signed-off-by: Andrey <andrii.tykhanovych.pl@gmail.com>

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    private static final String URL = "https://api.adviceslip.com/";
+
+    public static void main(String[] args) {
+
+        HttpClient httpClient = new HttpClient();
+        Slip slip = httpClient.fetch(URL+"advice", SlipResponse.class).getSlip();
+        System.out.println(slip.getAdvice());
+
+    }
+}
+
