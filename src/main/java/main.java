d768ba7a6dd8193eef674cf6be971b13804cf56d
Andrey <andrii.tykhanovych.pl@gmail.com>
2020-07-04 12:42:20 +0200

    Initial commit
    Signed-off-by: Andrey <andrii.tykhanovych.pl@gmail.com>

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -6,10 +8,15 @@ import java.net.URL;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hej");
+
 try {
     URL url = new URL("https://api.adviceslip.com/advice");
     HttpURLConnection connection = url.openConnection();
-    connection.setRequestMethod();
+    connection.setRequestMethod("Get");
+
+    BufferedReader in = new BufferedReader(new InputStreamReader())
+
+
 } catch () catch (MalformedURLException e) {
     e.printStackTrace();
 } catch (IOException e) {
