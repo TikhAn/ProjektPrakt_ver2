d7691b9f51b12d89d0bb033a9a5baaaba9b7f791
Andrey <andrii.tykhanovych.pl@gmail.com>
2020-07-04 12:39:55 +0200

    Initial commit
    Signed-off-by: Andrey <andrii.tykhanovych.pl@gmail.com>

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class main {
+    public static void main(String[] args) {
+        System.out.println("Hej");
+try {
+    URL url = new URL("https://api.adviceslip.com/advice");
+    HttpURLConnection connection = url.openConnection();
+    connection.setRequestMethod();
+} catch () catch (MalformedURLException e) {
+    e.printStackTrace();
+} catch (IOException e) {
+    e.printStackTrace();
+}
+
+    }
 }
