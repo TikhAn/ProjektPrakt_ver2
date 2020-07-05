20289d146fb8b4ef06877b7e1da864938a62667a
Andrey <andrii.tykhanovych.pl@gmail.com>
2020-07-05 11:09:36 +0200

    new new new
    Signed-off-by: Andrey <andrii.tykhanovych.pl@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Scanner;
 
-class Main {
+public class Main {
 
 
     public static void main(String[] args) {
 
-        Menu menu = new Menu(new AdviseService());
+        Menu menu = new Menu(new AdviceService());
         menu.displayMenu();
-
     }
 }
---
+++ src/main/java/http/SlipResponse.java
@@ -0,0 +1,19 @@
+
+package http;
+
+public class SlipResponse {
+
+    private SlipDto slip;
+
+    public SlipResponse(SlipDto slip) {
+        this.slip = slip;
+    }
+
+    public SlipDto getSlip() {
+        return slip;
+    }
+
+    public void setSlip(SlipDto slip) {
+        this.slip = slip;
+    }
+}
